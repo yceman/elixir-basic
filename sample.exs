a5c87b77bea7af1c543da26aa3fe09589e8ef359
Marcos Morais <marcosmorais.v10@gmail.com>
2025-05-18 00:16:31 -0300

    Add Sample file

---
+++ sample.exs
@@ -0,0 +1,23 @@
+number = 42
+"I am #{number} years old!"
+IO.puts("I am #{number} years old!")
+
+string = "world"
+IO.puts("hello #{string}!")
+
+IO.puts("hello\nworld")
+
+leng = String.length("hellö")
+IO.puts("#{leng}")
+
+bts = byte_size("hellö")
+IO.puts("#{bts}")
+
+isBinary = is_binary("hellö")
+IO.puts("#{isBinary}")
+
+upper = String.upcase("hellö")
+IO.puts("#{upper}")
+
+lower = String.downcase("HELLÖ")
+IO.puts("#{lower}")
